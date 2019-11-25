85ffe1b8bb4907608069952360d74f7fab72af1f
M Farkas-Dyck <strake888@gmail.com>
2019-11-24 20:05:06 -0800

    hoopl

--- src/Core.hs
+++ src/Core.hs
@@ -1,23 +1,33 @@
 {-# LANGUAGE StrictData #-}
+{-# LANGUAGE GADTs #-}
 module Core where
 
+import Compiler.Hoopl
+import Data.Kind (Type)
 import Numeric.Natural
 
-newtype Name = Name { unName :: Word }
-  deriving (Eq, Ord, Show)
+data Insn :: Type -> Type -> Type where
+    Label :: Label -> Insn C O
+    BumpStack :: Operand -> Insn O O
+    Read :: LogSize -> Operand -> Insn O O
+    Write :: LogSize -> Operand -> Operand -> Insn O O
+    UnOp :: UnOp -> Operand -> Insn O O
+    BinOp :: BinOp -> Operand -> Operand -> Insn O O
+    Branch :: BranchCmp -> Operand -> Operand -> Label -> Label -> Insn O C
+    UBranch :: Label -> Insn O C
+    Jump :: Operand -> [Operand] -> Insn O C
+    Unreachable :: Insn O C
 
-data Instruction
-  = Φ [(Operand, Name)]
-  | BumpStack Operand
-  | Read LogSize Operand
-  | Write LogSize Operand Operand
-  | UnOp UnOp Operand
-  | BinOp BinOp Operand Operand
+instance NonLocal Insn where
+    entryLabel (Label l) = l
+    successors = \ case
+        Branch _ _ _ l₁ l₂ -> [l₁, l₂]
+        UBranch l -> [l]
+        _ -> []
 
-data Terminator
-  = Branch BranchCmp Operand Operand Name Name
-  | Jump Operand [Operand]
-  | Unreachable
+instance HooplNode Insn where
+    mkBranchNode = UBranch
+    mkLabelNode = Label
 
 data UnOp
   = Ham | Clz | Ctz
@@ -38,11 +48,14 @@ data Signedness' = UU | SU | SS
 
 data Shift s = ShiftL | ShiftR s
 
-data Operand = Local Name | Const Const
+data Operand = Local Word | Const Const
 
 data Const = Literal Natural | Global Name
 
 data BranchCmp = Equal | Less Signedness
 
 newtype LogSize = LogSize { unLogSize :: Word }
+  deriving (Eq, Ord, Show)
+
+newtype Name = Name { unName :: [Char] }
   deriving (Eq, Ord, Show)
