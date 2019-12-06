a194acf88ef3d0193fa35bd6ecf052f5f784ff0f
M Farkas-Dyck <strake888@gmail.com>
2019-12-06 00:54:54 -0800

    x86 asm gen

---
+++ arch/x86/src/Asm.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+module Asm where
+
+import Compiler.Hoopl (Label)
+import Data.Bits
+import Data.Int (Int32, Int64)
+import Data.Kind
+
+import Core (LogSize)
+
+data Immediate a = Immediate a | LabelRelValue Label
+  deriving (Eq, Show)
+
+data Operand :: Bool -> Type where
+    ImmediateOperand :: Immediate Int64 -> Operand False
+    RegOperand :: Reg -> Operand rw
+    MemOperand :: LogSize -> Addr -> Operand rw
+    IPROperand :: LogSize -> Immediate Int32 -> Operand rw
+
+data Addr = Addr
+  { baseReg :: Maybe Reg
+  , displacement :: Immediate Int32
+  , indexReg :: Maybe (Scale, Reg)
+  } deriving (Eq)
+
+nullAddr :: Addr
+nullAddr = Addr { baseReg = Nothing, displacement = Immediate 0, indexReg = Nothing }
+
+data Scale = Scale1 | Scale2 | Scale4 | Scale8
+  deriving (Eq, Ord, Read, Show, Enum, Bounded)
+
+data Reg = Rax | Rcx | Rdx | Rbx | Rsp | Rbp | Rsi | Rdi | R8 | R9 | R10 | R11 | R12 | R13 | R14 | R15
+  deriving (Eq, Ord, Read, Show, Enum, Bounded)
+
+data Line where
+    Ret, Nop, PushF, PopF, Cmc, Clc, Stc, Cli, Sti, Cld, Std :: Line
+    Inc, Dec, Not, Neg, Bswap :: Operand True -> Line
+    Add, Or, Adc, Sbb, And, Sub, Xor, Cmp, Test, Mov, Bsf, Bsr, Andn, Imul :: Operand True -> Operand r -> Line
+    Rol, Ror, Rcl, Rcr, Shl, Shr, Sar :: Operand True -> Operand r -> Line
+    Popcnt, Lzcnt, Tzcnt :: Operand True -> Operand r -> Line
+    Cmov :: Cond -> Operand True -> Operand r -> Line
+    Set :: Cond -> Operand True -> Line
+
+    Lea :: Operand True -> Operand True -> Line
+
+    J :: Maybe Cond -> Operand r -> Line
+
+data Cond = O | NO | B | AE | E | NE | BE | A | S | NS | P | NP | L | GE | LE | G
+  deriving (Eq, Ord, Read, Show, Enum, Bounded)
+pattern NAE, C, NB, NC, Z, NZ, NA, NBE, PE, PO, NGE, NL, NG, NLE :: Cond
+pattern NAE = B
+pattern C   = B
+pattern NB  = AE
+pattern NC  = AE
+pattern Z   = E
+pattern NZ  = NE
+pattern NA  = BE
+pattern NBE = A
+pattern PE  = P
+pattern PO  = NP
+pattern NGE = L
+pattern NL  = GE
+pattern NG  = LE
+pattern NLE = G
+
+notCond :: Cond -> Cond
+notCond = toEnum . xor 1 . fromEnum
---
+++ arch/x86/src/AsmGen.hs
@@ -0,0 +1,139 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+module AsmGen where
+
+import Compiler.Hoopl hiding ((<*>))
+import Control.Applicative
+import Control.Monad (join)
+import Data.Bool (bool)
+import Data.Foldable (toList)
+import qualified Data.Map.Class as Map
+import qualified Data.Text as Text
+import Util
+import Util.List
+
+import Core hiding (Const)
+import qualified Core
+import Data.Assignment
+
+import Asm (Reg)
+import qualified Asm
+
+doGraphOC :: Graph (Assigned Reg (Insn Reg)) O C -> ([Asm.Line], LabelMap [Asm.Line])
+doGraphOC = \ case
+    GMany (JustO entry) body NothingO -> (doBlockOX entry, doBody body)
+
+doBody :: Body (Assigned Reg (Insn Reg)) -> LabelMap [Asm.Line]
+doBody = foldr (uncurry Map.insert . doBlockCC . snd) Map.empty . bodyList
+
+doBlockOX :: Block (Assigned Reg (Insn Reg)) O o -> [Asm.Line]
+doBlockOX = foldBlockNodesB3
+    ( \ _ -> id
+    , (<|>) . doInsnOO
+    , (<|>) . doInsnOC
+    ) <*> blockShape & snd & join shape ([] :: [Asm.Line])
+
+doBlockCC :: Block (Assigned Reg (Insn Reg)) C C -> (Label, [Asm.Line])
+doBlockCC = foldBlockNodesB3
+    (\ (Assigned (Argu (Const ())) (Label l)) -> (,) l
+    , (<|>) . doInsnOO
+    , (<|>) . doInsnOC
+    ) `flip` []
+
+doInsnOO :: Assigned Reg (Insn Reg) O O -> [Asm.Line]
+doInsnOO (Assigned (Lhs reg) insn) = case insn of
+    BumpStack a ->
+        [Asm.Mov x (Asm.RegOperand Asm.Rsp), Asm.Add (Asm.RegOperand Asm.Rsp) (doOperand a)]
+    UnOp op a -> [(case op of
+        Ham -> Asm.Popcnt
+        Clz -> Asm.Lzcnt
+        Ctz -> Asm.Tzcnt) x (doOperand a)]
+    BinOp op a b
+      | Local i <- a, reg == i -> doBinOp op x (doOperand b)
+      | Local j <- b, reg == j -> doBinOp op x (doOperand a)
+      | otherwise -> Asm.Mov x (doOperand a) : doBinOp op x (doOperand b)
+    Read lsz a -> [Asm.Mov x (Asm.MemOperand lsz (doAddr a))]
+    Write lsz a d -> [Asm.Mov (Asm.MemOperand lsz (doAddr a)) (doOperand d)]
+  where
+    x = Asm.RegOperand reg
+
+doInsnOC :: Assigned Reg (Insn Reg) O C -> [Asm.Line]
+doInsnOC (Assigned NoLhs insn) = case insn of
+    Branch _ (Core.Const _) _ _ _ -> error "branch"
+    Branch c (Core.Local i) b s t ->
+        [Asm.Cmp (Asm.RegOperand i) (doOperand b),
+         Asm.J (Just case c of
+                    Equal -> Asm.E
+                    Less False -> Asm.B
+                    Less True  -> Asm.L) (mkLabelOperand t),
+         Asm.J Nothing (mkLabelOperand s)]
+    UBranch l -> [Asm.J Nothing (mkLabelOperand l)]
+    Jump a as -> mkArguments (doOperand <$> as) ++ [Asm.J Nothing $ case a of
+        Core.Local k -> Asm.RegOperand k
+        Core.Const (Global (Right l)) -> mkLabelOperand l
+        Core.Const (Literal n) -> Asm.ImmediateOperand (Asm.Immediate (fromIntegral n))
+        Core.Const (Global (Left (Name name))) -> error ("name: " ++ Text.unpack name)]
+    Unreachable -> []
+
+mkLabelOperand :: Label -> Asm.Operand False
+mkLabelOperand = Asm.ImmediateOperand . Asm.LabelRelValue
+
+doOperand :: Operand Reg -> Asm.Operand False
+doOperand = \ case
+    Local k -> Asm.RegOperand k
+    Core.Const (Literal n) -> Asm.ImmediateOperand (Asm.Immediate (fromIntegral n))
+    Core.Const (Global (Right l)) -> Asm.ImmediateOperand (Asm.LabelRelValue l)
+    Core.Const (Global (Left (Name name))) -> error ("name: " ++ Text.unpack name)
+
+doAddr :: Operand Reg -> Asm.Addr
+doAddr = \ case
+    Local i -> Asm.nullAddr { Asm.baseReg = Just i }
+    Core.Const (Literal n) -> Asm.nullAddr { Asm.displacement = Asm.Immediate (fromIntegral n) }
+    Core.Const (Global (Right l)) -> Asm.nullAddr { Asm.displacement = Asm.LabelRelValue l }
+    Core.Const (Global (Left (Name name))) -> error ("name: " ++ Text.unpack name)
+
+doBinOp :: BinOp -> Asm.Operand True -> Asm.Operand r -> [Asm.Line]
+doBinOp = fmap sequenceA . sequenceA . \ case
+    Add    -> [Asm.Add]
+    Sub    -> [Asm.Sub]
+    Mul    -> [Asm.Imul]
+    XMul   -> error "xmul"
+    MulH _ -> error "mulh"
+    XMulH  -> error "xmulh"
+    Min s  -> [Asm.Cmp, Asm.Cmov (bool Asm.A Asm.G s)]
+    Max s  -> [Asm.Cmp, Asm.Cmov (bool Asm.B Asm.L s)]
+    And    -> [Asm.And]
+    Or     -> [Asm.Or]
+    Xor    -> [Asm.Xor]
+    Andc   -> [Asm.Andn]
+    Orc    -> [\ a _ -> Asm.Not a, Asm.And, \ a _ -> Asm.Not a]
+    Xnor   -> [Asm.Xor, \ a _ -> Asm.Not a]
+    Nand   -> [Asm.And, \ a _ -> Asm.Not a]
+    Nor    -> [Asm.Or,  \ a _ -> Asm.Not a]
+    Grev   -> error "grev"
+    Gorc   -> error "gorc"
+    Shfl   -> error "shfl"
+    Shift (ShiftL) -> [Asm.Shl]
+    Shift (ShiftR False) -> [Asm.Shr]
+    Shift (ShiftR True) -> [Asm.Sar]
+    Rotate (ShiftL) -> [Asm.Rol]
+    Rotate (ShiftR ()) -> [Asm.Ror]
+    Div _  -> error "div"
+    Rem _  -> error "rem"
+    Slt s  -> [Asm.Cmp, \ a _ -> Asm.Set (bool Asm.B Asm.L s) a]
+
+mkSwap :: Asm.Operand True -> Asm.Operand True -> [Asm.Line]
+mkSwap a b = [Asm.Xor a b, Asm.Xor b a, Asm.Xor a b]
+
+mkArguments :: [Asm.Operand r] -> [Asm.Line]
+mkArguments =
+    zipWithRemaining (compose2 Asm.Mov Asm.RegOperand id) [Asm.Rax ..] & \ (ls, r) -> ls ++ case r of
+    Just (Right as) ->
+        Asm.Add (Asm.RegOperand Asm.Rsp)
+                (Asm.ImmediateOperand (Asm.Immediate (fromIntegral $ 8*length as))) :
+        [Asm.Mov (Asm.MemOperand (LogSize 3)
+                  Asm.nullAddr { Asm.baseReg = Just Asm.Rsp
+                               , Asm.displacement = Asm.Immediate (8*k) }) a
+        | (k, a) <- count (toList as)]
+    _ -> []
