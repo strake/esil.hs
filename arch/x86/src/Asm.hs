5046527379c55d01570c9ae2ba3987d9a5fdf1a7
M Farkas-Dyck <strake888@gmail.com>
2020-01-04 19:04:26 -0800

    actually pretty-print x86 asm

--- arch/x86/src/Asm.hs
+++ arch/x86/src/Asm.hs
@@ -1,15 +1,25 @@
 {-# LANGUAGE StrictData #-}
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -ddump-splices #-}
 module Asm where
 
-import Compiler.Hoopl (Label)
+import Compiler.Hoopl.Label
+import Control.Applicative
+import Control.Monad (guard)
+import Control.Traversal.TH (makeTraversal)
 import Data.Bits
-import Data.Char (toLower)
+import Data.Char (isAlpha, toLower)
+import Data.Filtrable
 import Data.Int (Int32, Int64)
 import Data.Kind
+import Data.Monoid (Alt (..))
 import Data.Text.Prettyprint.Doc (Pretty (..))
 import qualified Data.Text.Prettyprint.Doc as Pretty
 
@@ -17,6 +27,11 @@ import Core (LogSize)
 
 data Immediate a = Immediate a | LabelRelValue Label
   deriving (Eq, Show)
+
+instance Pretty a => Pretty (Immediate a) where
+    pretty = \ case
+        Immediate a -> pretty a
+        LabelRelValue l -> pretty (lblToUnique l)
 
 data Operand :: Bool -> Type where
     ImmediateOperand :: Immediate Int64 -> Operand False
@@ -26,20 +41,35 @@ data Operand :: Bool -> Type where
 
 deriving instance Show (Operand rw)
 
+instance Pretty (Operand rw) where
+    pretty = \ case
+        ImmediateOperand n -> pretty n
+        RegOperand r -> pretty (toLower <$> show r)
+        MemOperand _ a -> pretty a
+        IPROperand _ _ -> mempty
+
 data Addr = Addr
   { baseReg :: Maybe Reg
   , displacement :: Immediate Int32
   , indexReg :: Maybe (Scale, Reg)
   } deriving (Eq, Show)
 
+instance Pretty Addr where
+    pretty Addr {..} = (Pretty.encloseSep "[" "]" " + " . catMaybes)
+        [pretty <$> baseReg, (\ (scale, reg) -> pretty scale <> "*" <> pretty reg) <$> indexReg,
+         pretty displacement <$ guard (displacement /= Immediate 0)]
+
 nullAddr :: Addr
 nullAddr = Addr { baseReg = Nothing, displacement = Immediate 0, indexReg = Nothing }
 
 data Scale = Scale1 | Scale2 | Scale4 | Scale8
   deriving (Eq, Ord, Read, Show, Enum, Bounded)
+instance Pretty Scale where
+    pretty = pretty . dropWhile isAlpha . show
 
 data Reg = Rax | Rcx | Rdx | Rbx | Rsp | Rbp | Rsi | Rdi | R8 | R9 | R10 | R11 | R12 | R13 | R14 | R15
   deriving (Eq, Ord, Read, Show, Enum, Bounded)
+instance Pretty Reg where pretty = pretty . fmap toLower . show
 
 data Line where
     Ret, Nop, PushF, PopF, Cmc, Clc, Stc, Cli, Sti, Cld, Std :: Line
@@ -74,10 +104,23 @@ pattern NGE = L
 pattern NL  = GE
 pattern NG  = LE
 pattern NLE = G
+instance Pretty Cond where pretty = pretty . fmap toLower . show
 
 notCond :: Cond -> Cond
 notCond = toEnum . xor 1 . fromEnum
 
+pure []
+
+operands :: ∀ p . (Applicative p) => (∀ rw . Operand rw -> p (Operand rw)) -> Line -> p Line
+operands f = $(do operandType <- [t|Operand|]; fExpr <- [|f|]; makeTraversal operandType fExpr ''Line)
+
 instance Pretty Line where
-    pretty = \ case
-        i -> pretty . fmap toLower . show $ i
+    pretty l =
+        insnDoc Pretty.<+>
+        (Pretty.encloseSep "" "" ", " . getAlt . getConst . operands (Const . pure . pretty)) l
+      where
+        insnDoc = pretty $ case l of
+            J condMay _ -> "j" ++ case condMay of
+                Nothing -> "mp"
+                Just cond -> toLower <$> show cond
+            _ -> takeWhile (/= ' ') $ toLower <$> show l
---
+++ arch/x86/x/emit-asm.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
+module Main where
+
+import Prelude hiding (lex)
+import Compiler.Hoopl
+import Control.Arrow
+import qualified Control.Monad.Free as Free
+import Control.Monad.Trans.Except (runExcept)
+import Data.Bool (bool)
+import Data.Char
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.Map.Class as MC
+import Data.Monoid (Alt (..))
+import Data.Void (absurd)
+import Data.Text (Text)
+import Data.Text.Prettyprint.Doc as Pretty
+import Data.Text.Prettyprint.Doc.Render.String
+import Core.RegAlloc
+import Text.Earley
+import Text.Lexer (defaultLexerSpec)
+import qualified Text.Lexer
+import qualified Text.Regex.Applicative as RE
+import Util
+
+import Core hiding (Const)
+import qualified Parse
+import qualified Asm
+import qualified AsmGen as Asm
+
+main :: IO ()
+main = interact $ doParse & \ case
+    (a:_, _) -> case runExcept $ (traverse . traverse) (allocRegs 15 (NothingC :: _ Label)) a of
+        Left e -> error (show e)
+        Right a' ->
+            renderString $ layoutPretty LayoutOptions { layoutPageWidth = AvailablePerLine 96 (7/8) } $
+            Map.foldMapWithKey (fmap pretty . uncurry . Fn . unName) . fmap (either absurd Asm.doGraphOC) $
+            (fmap . fmap) (bimapGraphBinders (toReg . snd) (toReg . snd)) $ a'
+
+    (_, r) -> error (show r)
+
+toReg n = toEnum (n + bool 0 1 (n >= 4))
+
+doParse :: _ -> ([Map _ _], _)
+doParse = fullParses theParser . lex
+
+lex = fmap snd . fst . listFree . Text.Lexer.lex defaultLexerSpec { Text.Lexer.token = Parse.token, Text.Lexer.space = () <$ RE.psym ((==) Space . generalCategory) }
+
+listFree = getAlt *** id <<< Free.fold (\ (p, t, a) -> (Alt [(p, t)], a))
+
+theParser = parser Parse.grammar
+
+data Fn = Fn
+  { name :: Text
+  , entry :: [Asm.Line]
+  , body :: LabelMap [Asm.Line]
+  }
+
+instance Pretty Fn where
+    pretty (Fn { name, entry, body }) = (mconcat . fmap (<> Pretty.line))
+        (pretty name <> ":" :
+         fmap (("\t" <>) . pretty) entry ++
+         MC.foldrWithKey (\ (Lbl l) cs -> (++) $ ("." <> pretty l <> ":") : fmap pretty cs) [] body)
